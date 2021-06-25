336f3bcbd981518c36d0ee541d61fe29bd44bd58
Randall Krebs <randall.krebs@non.se.com>
2021-06-25 15:39:56 -0400

    retrieve log messages by running git with proper arguments

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,14 @@
 import * as core from '@actions/core'
 import  * as event from './event'
 import * as version from './version'
+import * as git from './git'
 
 export default async function run(): Promise<void> {
   try {
     const tag = event.getCreatedTag()
 
-    if (tag && version.isSemVer(tag)) {
-      // TODO:
+    if (tag && version.isSemVer(tag)) { 
+      const changelog = await git.getChangesIntroducedByTag(tag)
     }
 
     core.setOutput('release-url', 'https://www.krebsweb.com')
