442e7c3f731317e296782670ba5a54baae6146b6
Randall Krebs <randall.krebs@non.se.com>
2021-06-25 15:05:40 -0400

    check the event type and tag

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import * as core from '@actions/core'
+import  * as event from './event'
+import * as version from './version'
 
 export default async function run(): Promise<void> {
   try {
-    // Implementation
+    const tag = event.getCreatedTag()
+
+    if (tag && version.isSemVer(tag)) {
+      // TODO:
+    }
+
     core.setOutput('release-url', 'https://www.krebsweb.com')
   } catch (error) {
     core.setFailed(error.message)
